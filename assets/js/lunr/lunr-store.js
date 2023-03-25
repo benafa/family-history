790dccacdc9868df9226dfa182ab247bf1686a9e
Ben Anandappa <ben.anandappa@gmail.com>
2023-03-25 16:44:47 -0500

    Use miminal mistakes theme, create homepage and newspaper articles

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,163 @@
+var store = [{
+        "title": "Testementary of Abraham Peter Casie Chitty",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1953 Petitioner: Mary Clara Casie Chitty (and others) Petitioner reationship to deceased: Unclear Transcript IN THE DISTRICT COURT OF COLOMBO In the Matter of the Last Will and Testament of the late Abraham Peter Casie Chitty of Margate, Sea Avenue, Colpetty in Colombo, deceased Petitioners:...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/abraham-peter-casie-chitty-.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Ana Maria Agnes Saverimuttu (nee Anandappa)",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1909 Petitioner: Juliana Saverimuttu Pulle (nee Anandappa) Petitioner reationship to deceased: Administers estate of husband Transcript District Court of Colombo In the matter of the estate and effects of Ana Maria Agnes Saverimuttu nee Anandappa, late of Jampettah street in Colombo, deceased. Petitioner: Juliana Saverimuttu...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/ana-maria-agnes-saverimuttu-(nee-anandappa).html",
+        "teaser": null
+      },{
+        "title": "Testementary of Anthony Anandappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1906 Petitioner: Helena Perera Petitioner reationship to deceased: Wife (based on her own testementary) Transcript IN THE DISTRICT COURT OF COLOMBO In the Matter of the Last Will and Testament of Anthony Anandappa of Colombo, deceased. Helena Perera of Colombo, Petitioner. THIS matter came on...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/anthony-anandappa-.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Anthony Silva Candappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1906 Petitioner: Lucy Mary Casie Chetty Petitioner reationship to deceased: Daughter Transcript In the District Court of Colombo. In the Matter of the Estate of the late Anthony Silva Candappa, deceased, of No. 50, New Chetty street in Colombo This matter came up for disposal...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/anthony-silva-candappa.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Dominga Casie Chetty",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1906 Petitioner: Lucy Mary Casie Chetty Petitioner reationship to deceased: Daughter Transcript In the District Court of Colombo. In the Matter of the Estate of the late Dominga Casie Chetty, deceased, of Colombo. This matter came on for disposal before Felix Reginald Dias, Esq., Additional...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/dominga-casie-chetty.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Dominic Philip Anandappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1931 Petitioner: Joseph Jerome Theodore Anandappa Petitioner reationship to deceased: Eldest son Transcript In the District Court of Colombo. In the Matter of the Intestate Estate of Dominic Philip Anandappa of No. 33, Kotahena Street in Colombo, deceased. Petitioner: Joseph Jerome Theodore Anandappa of No....","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/dominic-philip-anandappa.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Emmanuel Anandappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1915 Petitioner: Maria Nathalia Anandappa Petitioner reationship to deceased: Wife Transcript In the District Court of Colombo, Letters of Administration In the Matter of the Intestate Estate of the deceased Emmanuel Anandappa of College street, Kotahena, in Colombo. Petitioner: Maria Nathalia Anandappa of New Chetty...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/emmanuel-anandappa.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Emmanuel Peiris Tavarayan",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1946 Petitioner: Theresa Peiris Tavarayan Petitioner reationship to deceased: Wife Transcript In the District Court of Colombo. In the Matter of the Intestate Estate of the late Emmanuel Peiris Tavarayan of 22, Galpotte street, Kotahena in Colombo, deceased. Petitioner: Theresa Peiris Tavarayan of 22, Galpotte...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/emmanuel-peiris-tavarayan.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Emmanuel Theodore Paschal Anandappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1927 Petitioner: Dominic Philip Anandappa Petitioner reationship to deceased: Father Transcript In the District Court of Colombo In the Matter of the Intestate Estate of the late Emmanuel Theodore Paschal Anandappa of Wellawatta, Colombo, deceased. Petitioner: Dominic Philip Anandappa of Wellawatta in Colombo Respondent: Maria...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/emmanuel-theodore-paschal-anandappa-.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Francis Xavier Adaman",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1900 Petitioner: John Bonafacio Misso Petitioner reationship to deceased: Unclear Transcript In the District Court of Kandy. In the Matter of the Estate of the late Francis Xavier Adaman, of Cemetery street, Kotahena, Colombo, and late of Kandy, deceased. This matter coming on for disposal...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/francis-xavier-adaman.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Gabriel Simon Casie Chetty",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1923 Petitioner: Annee Casie Chetty Petitioner reationship to deceased: Wife Transcript In the District Court of Colombo. In the Matter of the Intestate Estate of Gabriel Simon Casie Chetty, late Matara, deceased. Petitioner: Annee Casie Chetty of Mount Lavinia Respondents (1) Gladys Cecilia Casie Chetty,...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/gabriel-simon-casie-chetty.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Helena Perera",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1923 Petitioner: Xavier Anandappa Petitioner reationship to deceased: Brother of her husband Transcript In the District Court of Colombo. In the Matter of the Interstate Estate and Effects of Helena Perera of Hisella in Werahera in the Ragam pattu of Alutkuru, deceased. Petitioner: Xavier Anandappa...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/helena-perera.html",
+        "teaser": null
+      },{
+        "title": "Testementary of John Caitan Anthony Casie Chetty",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1911 Petitioner: Ana Casie Chetty Petitioner reationship to deceased: Wife Transcript In the District Court of Colombo. In the Matter of the Property and Estate, Rights, and Credits of John Caitan Anthony Casie Chetty, deceased. Petitioner: Ana Casie Chetty, widow of the said John Casie...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/john-caitan-anthony-casie-chetty.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Joseph Jerome Theodore Anandappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1935 Petitioner: Mary Magdeline Anandappa Petitioner reationship to deceased: Unclear Transcript In the District Court of Colombo. In the Matter of the Intestate Estate of Joseph Jerome Theodore Anandappa of 89, 2nd Marshall street, Mutwal, in Colombo. Petitioner: Mary Magdeline Anandappa of 145, Jampettah street...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/joseph-jerome-theodore-anandappa-.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Juliana Saverimuttu Pulle",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1912 Petitioner: Francis Silva Candappa Petitioner reationship to deceased: Unclear Transcript In the District Court of Colombo. In the Matter of the Last Will and Testament of the late Juliana Saverimuttu Pulle (widow of the late Nicholas Saverimuttu Pulle), of New Chetty street, Colombo, deceased....","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/juliana-saverimuttu-pulle-.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Lucy Mary Casie Chetty",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1955 Petitioner: Antony Vincent Casie Chetty Petitioner reationship to deceased: Son Transcript In the District Court of Colombo In the Matter of the Intestate Estate of Lucy Mary Casie Chetty Petitioner: Antony Vincent Casie Chetty of 146, New Chetty Street, Colombo Respondents: (1) Gasper Joseph...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/lucy-mary-casie-chetty.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Maria Nathalia Anandappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1925 Petitioner: Christofel Lazarus Anandappa Petitioner reationship to deceased: Son Transcript In the District Court of Colombo. In the Matter of the Intestate Estate of Maria Nathalia Anandappa of New Chetty street in Colombo, deceased. Petitioner: Christofel Lazarus Anandappa of New Chetty street in Colombo....","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/maria-nathalia-anandappa-.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Maria Pieris Tevarayan",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1924 Petitioner: Caitan Pieris Tevarayan Petitioner reationship to deceased: Brother Transcript In the District Court of Colombo. In the Matter of the Intestate Estate of Maria Pieris Tevarayan, late of New Street, in Colombo, deceased. Petitioner: Caitan Pieris Tevarayan of Hendala Respondents (1) Antony Xavier...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/maria-pieris-tevarayan.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Maria Salome Sangaramurthiapillai (nee Anandappa)",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1905 Petitioner: Joseph Bernard Sangaramurthiapillai Petitioner reationship to deceased: Unclear Transcript In the District Court of Colombo. In the Matter of the Estate of the late Maria Salome Sangaramurthiapillai, nee Anandappa, deceased, of Kelaniya in Colombo. THIS matter coming on for disposal before James Richard...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/maria-salome-sangaramurthiapillai-(nee-anandappa).html",
+        "teaser": null
+      },{
+        "title": "Testementary of Mariano Leity Ramanaden",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1927 Petitioner: Philippa Adaman Petitioner reationship to deceased: Unclear Transcript In the District Court of Colombo. In the Matter of the Last Will and Testament of Mariano Leity Ramanaden of Poo Villa, Jampettah street in Colombo, deceased. Petitioner: Philippa Adaman of Jampetta Street in Colombo...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/mariano-leity-ramanaden-.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Mary Teresa Casie Chitty",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1920 Petitioner: Abraham Peter Casie Chitty Petitioner reationship to deceased: Son Transcript In the District Court of Colombo In the Matter of the Intestate Estate of Mary Teresa Casie Chitty of Tereselyn, Jampettah street, Kotahena, in Colombo. Petitioner: Abraham Peter Casie Chitty of Tereselyn, Jampettah...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/mary-teresa-casie-chitty.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Pasqual Fernandopulle",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1920 Petitioner: Joseph Louis Fernandopulle Petitioner reationship to deceased: Brother Transcript In the District Court of Colombo. In the Matter of the Intestate Estate of Pasqual Fernandopulle of New Chetty street, Colombo, deceased. Petitioner: Joseph Louis Fernandopulle of New Chetty street, Colombo, Petitioner, Respondents (1)...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/pasqual-fernandopulle.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Peter Benedict Anandappa",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1916 Petitioner: Caetan Louis Anandappa Petitioner reationship to deceased: Son Transcript In the District Court of Colombo. Testamentary In the Matter of the Intestate Estate of the late Peter Benedict Anandappa, late of No. 39, Jampettah street, Colombo, deceased. Petitioner: Caetan Louis Anandappa of No....","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/peter-benedict-anandappa.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Peter Wilmot de Rosairo",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1919 Petitioner: Teresa Mary de Rosairo Petitioner reationship to deceased: Wife Transcript In the District Court of Puttalam. In the Matter of the Interstate Estate of Peter Wilmot de Rosairo, late of Puttlam, deceased Peititoner: Teresa Mary de Rosairo of Puttlam, widow of the deceased...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/peter-wilmot-de-rosairo.html",
+        "teaser": null
+      },{
+        "title": "Testementary of Philippa Annandappa (nee Masillamani)",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1902 Petitioner: Maria Salome Anandappa Petitioner reationship to deceased: Unclear Transcript In the District Court of Colombo. In the matter of the Estate of the Last Will and Testament of Philippa Annandappa, nee Masillamani, deceased, of New Chetty Sea Street, Colombo. The matter coming on...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/philippa-annandappa-(nee-masillamani).html",
+        "teaser": null
+      },{
+        "title": "Testementary of Saviel Pieris Tavarayen",
+        "excerpt":"Source: Ceylon Government Gazette Year: 1897 Petitioner: Cecilia Rodrigo Petitioner reationship to deceased: Wife Transcript In the District Court of Colombo. In the Matter of the Estate of Saviel Pieris Tavarayen, late of No. 42, New Chetty street in Colombo, deceased This matter came on for disposal before Felix Reginald...","categories": ["newspaper","gazette"],
+        "tags": [],
+        "url": "/family-history/newspaper/saviel-pieris-tavarayen.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/family-history/jekyll/update/2023/03/05/welcome-to-jekyll.html",
+        "teaser": null
+      },]
